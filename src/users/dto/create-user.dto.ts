1b765ca24c7fbc72d6f5f6c473557ad1d19b0bc4
Leonardo Santos <leonardosaint748@gmail.com>
2022-02-26 22:38:16 -0300

    feat: add validator to create user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,41 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  IsBoolean,
+  isNotEmpty,
+  MinLength,
+  MaxLength,
+  Matches,
+  IsOptional,
+} from 'class-validator';
 import { User } from '../entities/user.entity';
 
 export class CreateUserDto extends User {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsString()
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   password: string;
+
+  @IsBoolean()
+  @IsOptional()
   enabled?: boolean;
+
+  @IsString()
+  @IsOptional()
   description?: string;
 }
